40abeaaa75bab1f99505873cc69c664637ddc5d3
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2020-07-05 13:14:05 +0200

    update kubernetes-mod.sh script and run it

--- scripts/kubernetes-mod.sh
+++ scripts/kubernetes-mod.sh
@@ -3,6 +3,20 @@
 # Workaround issues importing argocd library importing k8s stuff
 # See: https://github.com/kubernetes/kubernetes/issues/79384#issuecomment-521493597
 #
+#
+# Here's an example of this issue:
+#
+#  ➜ go test ./pkg/gitops 
+# go: finding module for package github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1
+# go: finding module for package github.com/smartystreets/goconvey/convey
+# go: finding module for package github.com/google/uuid
+# go: found github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1 in github.com/argoproj/argo-cd v1.6.1
+# go: found github.com/google/uuid in github.com/google/uuid v1.1.1
+# go: found github.com/smartystreets/goconvey/convey in github.com/smartystreets/goconvey v1.6.4
+# go: github.com/argoproj/argo-cd@v1.6.1 requires
+#    k8s.io/kubernetes@v1.16.6 requires
+#    k8s.io/api@v0.0.0: reading k8s.io/api/go.mod at revision v0.0.0: unknown revision v0.0.0
+#
 
 set -euo pipefail
 
