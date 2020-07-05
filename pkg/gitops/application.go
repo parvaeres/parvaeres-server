2b5686d218da11134fd2299e88bf9092804cb29b
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2020-07-05 13:14:43 +0200

    create an ArgoCD application with populated fields

--- pkg/gitops/application.go
+++ pkg/gitops/application.go
@@ -3,25 +3,58 @@ package parvaeres
 import (
 	"github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
 	"github.com/google/uuid"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func getDefaultApplication() *v1alpha1.Application {
+	return &v1alpha1.Application{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "defaultApplication",
+			Namespace: "default",
+			Annotations: map[string]string{
+				"parvaeres-email":   "",
+				"parvaeres-repoURL": "",
+			},
+		},
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "argoproj.io/v1alpha1",
+			Kind:       "Application",
+		},
+		Spec: v1alpha1.ApplicationSpec{
+			Project: "default",
+			Source: v1alpha1.ApplicationSource{
+				Path:           "",
+				TargetRevision: "HEAD",
+				RepoURL:        "",
+			},
+			Destination: v1alpha1.ApplicationDestination{
+				Namespace: "default",
+				Server:    "https://kubernetes.default.svc",
+			},
+			SyncPolicy: &v1alpha1.SyncPolicy{
+				Automated: &v1alpha1.SyncPolicyAutomated{
+					Prune:    true,
+					SelfHeal: true,
+				},
+				SyncOptions: v1alpha1.SyncOptions{},
+			},
+		},
+	}
+}
+
+// GenerateApplication returns an ArgoCD Application relative to email and repoURL
 func GenerateApplication(email string, repoURL string) (*v1alpha1.Application, error) {
 
-	// generate UUID
-	// create Deployment{email, repository, UUID, created_at_timestamp, status}
-	// store Deployment in data store
-	// create corresponding ArgoCD Application object
-	// write it in k8s
-
-	id, err := uuid.NewUUID()
+	id, err := uuid.NewRandom()
 	if err != nil {
 		return nil, err
 	}
 
-	newApplication := v1alpha1.Application{}
-	newApplication.Name = id.String()
-	newApplication.Annotations["parvaeres-email"] = email
-	newApplication.Annotations["parvaeres-repoURL"] = repoURL
+	newApplication := getDefaultApplication()
+	newApplication.Spec.Source.RepoURL = repoURL
+	newApplication.ObjectMeta.Name = id.String()
+	newApplication.ObjectMeta.Annotations["parvaeres-email"] = email
+	newApplication.ObjectMeta.Annotations["parvaeres-repoURL"] = repoURL
 
-	return &newApplication, nil
+	return newApplication, nil
 }
--- pkg/gitops/application_test.go
+++ pkg/gitops/application_test.go
@@ -3,24 +3,28 @@ package parvaeres
 import (
 	"testing"
 
-	"github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
+	"github.com/google/uuid"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 //TestApplication tests the application generation
-func TestApplication(t *testing.T) {
-
-	expectedApplication := v1alpha1.Application{}
-	expectedApplication.Name = "something"
+func TestGenerateApplication(t *testing.T) {
 
 	Convey("Given a url and an email", t, func() {
 		inputURL := "http://blabla"
 		inputEmail := "my@email.com"
 		Convey("When creating an Application", func() {
-			//the function to test
-			newApplication, err := generateApplication(inputEmail, inputURL)
-			Convey("Then the Application is created as expected", func() {
-				So(expectedApplication, ShouldEqual, newApplication)
+			newApplication, err := GenerateApplication(inputEmail, inputURL)
+			Convey("Then the Application fields are populated as expected", func() {
+				So(err, ShouldBeNil)
+				So(newApplication.Spec.Source.RepoURL, ShouldEqual, inputURL)
+				So(newApplication.ObjectMeta.Annotations["parvaeres-email"], ShouldEqual, inputEmail)
+				So(newApplication.ObjectMeta.Annotations["parvaeres-repoURL"], ShouldEqual, inputURL)
+				Convey("And the name field is a UUID", func() {
+					uuid, err := uuid.Parse(newApplication.ObjectMeta.Name)
+					So(err, ShouldBeNil)
+					So(uuid.Version().String(), ShouldEqual, "VERSION_4")
+				})
 			})
 		})
 	})
