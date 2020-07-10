3d1980f37752b656832558b40b393cd4f0399090
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2020-07-10 15:06:58 +0200

    add scripts to create k3d cluster with docker registy

--- platform/deploy-on-k3s.sh
+++ platform/deploy-on-k3s.sh
@@ -5,5 +5,3 @@ CLUSTER_NAME=parvae
 k3d create -n $CLUSTER_NAME -w 3 --enable-registry
 export KUBECONFIG="$(k3d get-kubeconfig --name=$CLUSTER_NAME)"
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
